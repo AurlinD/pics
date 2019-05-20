ab9c050d1fc2332b4c0f851fe182a90a33e319f1
Aurlin Dhillon <aurlindhillon@gmail.com>
2019-05-19 22:01:50 -0700

    API call

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   // passing props that is coming from CHILD to PARENT.
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 94aee17ebe7b7ecd7e462d7ce60ae9f51ae8cb36b6b29396010e815728c10f89"
+      }
+    });
   }
   render() {
     return (

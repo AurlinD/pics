96509d55a735de1bf45879f22b6f5bb5d2c60959
Aurlin Dhillon <aurlindhillon@gmail.com>
2019-06-01 18:58:21 -0700

    Updates

---
+++ precache-manifest.827c63d334923ee4c479ab093ef06e1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc087c11046da8b012562b3ee211ca3",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "9890633e52d764400fee",
+    "url": "/pics/static/css/main.744c5823.chunk.css"
+  },
+  {
+    "revision": "3db3777322b5d619c68a",
+    "url": "/pics/static/js/2.1b3f01f7.chunk.js"
+  },
+  {
+    "revision": "9890633e52d764400fee",
+    "url": "/pics/static/js/main.ab4d76d3.chunk.js"
+  },
+  {
+    "revision": "9198ae3b868f769fc8ea",
+    "url": "/pics/static/js/runtime~main.7218180c.js"
+  }
+]);
